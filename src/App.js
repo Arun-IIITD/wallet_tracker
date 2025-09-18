f137f74fcc24f6001b0abc5fb3a20358e61ef5a2
Arun Yadav <ay20552291@gmail.com>
2025-09-18 09:46:22 +0530

    second

--- src/App.js
+++ src/App.js
@@ -1,23 +1,197 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
 function App() {
+  const categories = ["Food", "Entertainment", "Travel"];
+  const [walletbalance, setwalletbalance] = useState(5000);
+  const [expenses, setexpenses] = useState(0);
+  const [transactions, setTransaction] = useState("");
+  const [showIncomeModal, setshowincomemodal] = useState("");
+  const [showexpensemodal, setshowexpensemodal] = useState("");
+  const [incomeAmount, setIncomeAmount] = useState("");
+  const [expenseform, setexpenseform] = useState({
+    title: "",
+    price: "",
+    category: "food",
+    date: "",
+  });
+
+    //FETCH FROM LOCAL STORAGE
+   useEffect(() => {
+  const wallet_balance = localStorage.getItem("walletbalance")
+  const wallet_expenses = localStorage.getItem("expenses")
+
+  if (wallet_balance){
+    setwalletbalance(Number(wallet_balance))
+  }
+
+  if (wallet_expenses){
+    setexpenses(Number(wallet_expenses))
+  }
+
+  const wallet_transactions = localStorage.getItem("transactions");
+  if (wallet_transactions){
+    setTransaction(wallet_transactions)
+  }
+   }, []);
+
+
+
+  //ADD TO LOCAL STORAGE
+  useEffect(() => {
+    localStorage.setItem("walletbalance", Number(walletbalance))
+    localStorage.setItem("expense", Number(expenses))
+      localStorage.setItem("transactions", transactions);
+  }, [walletbalance, expenses, transactions])
+
+  //ADD BALANCE OR INCOME
+  const handleaddincome = (e) => {
+    e.preventDefault();
+    if(!incomeAmount) return;
+
+    setwalletbalance(Number(walletbalance) + Number(incomeAmount));
+    setIncomeAmount("");
+    setshowincomemodal(false);
+}
+
+//ADD EXPENSES
+const handleaddexpense = (e) => {
+  e.preventDefault();
+
+  const {title, price, category, date} = expenseform;
+  if (!title || !price || !category || !date){
+    return ;
+  }
+
+  const newexpense = {
+   id: Date.now(),
+   title,
+   price: Number(price),
+   category,
+   date,
+  };
+
+  setTransaction([...transactions,newexpense]);
+  setexpenses(expenses +Number(price))
+  setwalletbalance(walletbalance - Number(price))
+  setexpenseform("");
+  setshowexpensemodal(false)
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Expense Tracker</h1>
+
+      <div className="cards">
+          <div className="card">
+            <h2>Wallet Balance:  <span style={{ color: "limegreen" }}>₹{walletbalance}</span> </h2>
+            <button onClick={()=> setshowincomemodal(true)} style = {{backgroundColor: "limegreen"}}> + Add Balance</button>
+          </div>
+
+
+          <div className="card">
+            <h2>Expenses: <span style={{ color: "orange" }}>₹{expenses}</span> </h2>
+            <button onClick={() => setshowexpensemodal(true)} style = {{backgroundColor: "orange"}}> + Add Expenses</button>
+          </div>
+
+      </div>
+
+        {/* ADD BALANCE POP UP */}
+       {showIncomeModal && (
+      <div className="modal">
+     
+      <h2>ADD BALANCE</h2>
+      <form onSubmit={handleaddincome}>
+        <input
+        type="number"
+        placeholder="Income Amount"
+        value={incomeAmount}
+        onChange= {(e) => setIncomeAmount(e.target.value)}
+        />
+
+        <button type="submit">Add Balance</button>
+        <button type="button" onClick={()=> setshowincomemodal(false)}>Cancel</button>
+
+      </form>
+      </div>
+       )}
+
+       {/* EXPENSE POP UP */}
+       {showexpensemodal && 
+       <div className="modal">
+          <h2>ADD EXPENSES</h2>
+        <form onSubmit={handleaddexpense}>
+          {/* FOR TITLE */}
+          <input
+          name="title"
+          placeholder="TITLE"
+          value={expenseform.title}
+          onChange={(e) => setexpenseform({...expenseform,title: e.target.value})}
+          />
+           {/* FOR PRICE */}
+          <input
+          name="price"
+          type="number"
+          placeholder="price"
+          value={expenseform.price}
+           onChange={(e) => setexpenseform({ ...expenseform, price: e.target.value })}
+          />
+          
+           {/* FOR CATEGORIES */}
+          <select
+          name="category"
+          value={expenseform.category}
+           onChange={(e) => setexpenseform({ ...expenseform, category: e.target.value })}
+          >
+            {categories.map((cat,index) => (
+              <option key={index} value={cat}>{cat}</option>
+            ))}
+
+        </select>
+
+         {/* FOR DATE */}
+         <input
+         name="date"
+         type ="date"
+         value={expenseform.date}
+          onChange={(e) => setexpenseform({ ...expenseform, date: e.target.value })}
+          
+         />
+
+        
+        <button type="submit">Add Expense</button>
+        <button onClick={() => setshowexpensemodal(false)}>Cancel</button>
+
+
+        </form>
+       </div>
+      }
+
+       {/* RECENT TRANSACTIONS*/}
+       <div className="transactions">
+
+       </div>
+
+
+
+
+
+
+
     </div>
   );
 }
